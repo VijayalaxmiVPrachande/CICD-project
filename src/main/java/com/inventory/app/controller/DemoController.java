7debbb644bccc742cefefa8128195607999d4b22
vijayalaxmi prachande <vijuvprachande@gmail.com>
2026-07-02 14:23:31 +0530

    Initial commit

---
+++ src/main/java/com/inventory/app/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.inventory.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	
+	@GetMapping("/hi")
+	public String Welcome() {
+		return "Welcome to my CICD Project ";
+	}
+}
